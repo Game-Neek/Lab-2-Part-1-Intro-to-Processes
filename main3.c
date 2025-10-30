17717f3ac962fb8840a6c34c3686b1bb73aed508
Game-Neek <33062785+Game-Neek@users.noreply.github.com>
2025-10-30 15:40:28 -0400

    Include headers for process and time functions
    Added necessary headers for process control and time functions.

--- main3.c
+++ main3.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
 //get wait sys call include line
 
 void child_process();
